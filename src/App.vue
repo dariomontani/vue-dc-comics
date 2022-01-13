70c465e46ec7709a176d0d3442def91a6aa27c5f
dariomontani <dariomontani@gmail.com>
2022-01-13 17:57:47 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <Header></Header>
     <Main></Main>
     <FooterUp></FooterUp>
@@ -33,12 +32,5 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/scss/style.scss"
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,19 @@
 <template>
-  <header>
-      <img src="../asset/img/dc-logo-bg.png" alt="">
+  <div class="container">
+      <img src="../assets/img/dc-logo.png" alt="">
       <ul>
-          <li>CHARACTERS</li>
-          <li>COMICS</li>
-          <li>MOVIES</li>
-          <li>TV</li>
-          <li>GAMES</li>
-          <li>COLLECTIBLES</li>
-          <li>VIDEOS</li>
-          <li>FANS</li>
-          <li>NEWS</li>
-          <li>SHOP</li>
+          <li><a href="#">CHARACTERS</a></li>
+          <li><a href="#">COMICS</a></li>
+          <li><a href="#">MOVIES</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">GAMES</a></li>
+          <li><a href="#">COLLECTIBLES</a></li>
+          <li><a href="#">VIDEOS</a></li>
+          <li><a href="#">FANS</a></li>
+          <li><a href="#">NEWS</a></li>
+          <li><a href="#">SHOP</a></li>
       </ul>
-  </header>
+  </div>
 </template>
 
 <script>
@@ -26,7 +26,32 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    img {
+        display: block;
+    }
+    div {
+        padding-top: 2em;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        ul{
+            list-style: none;
+            display: flex;
+            align-items: center;
+            li{
+                padding-left: 2em;
+                a{
+                    text-decoration: none;
+                    color: black;
+                    &:hover,
+                    &:active {
+                        color: #0c7cec;
+                        border-bottom: 2px solid #0c7cec;
+                    }
+                }
+            }
+        }
+    }
 </style>
 
