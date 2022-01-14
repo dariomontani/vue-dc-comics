30f9c7c6a28cecf7209af0acdbbcf43e29f626f6
dariomontani <dariomontani@gmail.com>
2022-01-14 16:55:48 +0100

    inserimento immagini dinamico

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card">
+        <img :src="image" alt="">
+        <h3>{{title}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Card",
+    props: [
+        "image",
+        "title",
+    ],
+    data(){
+        return{}
+    }
+}
+</script>
+
+<style>
+
+</style>
