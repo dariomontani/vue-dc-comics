e5e500b453fbb7b36e6dd28f5b0acd9ba8189597
dariomontani <dariomontani@gmail.com>
2022-01-14 18:40:17 +0100

    size jumbo

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -105,7 +105,7 @@ export default {
 <style lang="scss" scoped>
 main {
   .containerJumbo{
-    height: 400px;
+    height: 350px;
     overflow: hidden;
     display: flex;
     justify-content: center;
